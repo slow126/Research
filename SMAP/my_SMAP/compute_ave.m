00fc5ebaace8bd1308ba4ee15fdc688e0decd87a
slow126 <spencerlow126@me.com>
2020-10-03 22:28:13 -0600

    update

--- SMAP/my_SMAP/compute_ave.m
+++ SMAP/my_SMAP/compute_ave.m
@@ -1,18 +1,11 @@
-function [b_val, sy] = compute_ave(pow, count, fill_array, response_array, b_val, sy)
+function [ave] = compute_ave(pow, fill_array, response_array, a_val)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-my_count = zeros(size(b_val,1),size(b_val,2));
+    ave = zeros(size(a_val));
     for i = 1:length(response_array)
-%         num(i) = sum(response_array(i).resp);        
-        n = fill_array(i).pt;
-        m = response_array(i).resp;
-        b_val(n) = b_val(n) + pow(i) * m;
-        my_count(n) = my_count(n) + 1;
-        
+        ave(fill_array(i).pt) = nansum(pow(i) .* response_array(i).resp) ./ nansum(response_array(i).pt);
     end
-    b_val = b_val ./ (my_count);
     
-    measurement * response / sum(response)
     
 end
 
